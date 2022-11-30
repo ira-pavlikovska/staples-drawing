fe6e0234a1872441819df9ecce39e7388aa91b4e
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2022-11-30 16:59:09 -0700

    toolbar styles

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,25 +3,31 @@ import IconButton from '@mui/material/IconButton';
 import {NoteAdd, Undo, Redo, Delete} from '@mui/icons-material';
 import { useDispatch } from 'react-redux';
 import {newDrawingDocument, undo, redo, deleteSelectedShape} from "../reducer/drawingReducer";
+import Grid from "@mui/system/Unstable_Grid";
 
 export default function Toolbar() {
     const dispatch = useDispatch()
 
     return (
-        <div>
-            <IconButton onClick={() => dispatch(newDrawingDocument())} color="primary" aria-label="New Document" component="label">
-                <NoteAdd/>
-            </IconButton>
-
-            <IconButton onClick={() => dispatch(undo())} color="primary" aria-label="Undo" component="label">
-                <Undo/>
-            </IconButton>
-            <IconButton onClick={() => dispatch(redo())} color="primary" aria-label="Redo" component="label">
-                <Redo/>
-            </IconButton>
-            <IconButton onClick={() => dispatch(deleteSelectedShape())} color="primary" aria-label="Delete selected shape" component="label">
-                <Delete/>
-            </IconButton>
-        </div>
+        <Grid container spacing={1}>
+            <Grid xs={1}>
+                <IconButton onClick={() => dispatch(newDrawingDocument())} color="primary" aria-label="New Document" component="label">
+                    <NoteAdd/>
+                </IconButton>
+            </Grid>
+            <Grid xs={10}>
+                <IconButton onClick={() => dispatch(undo())} color="primary" aria-label="Undo" component="label">
+                    <Undo/>
+                </IconButton>
+                <IconButton onClick={() => dispatch(redo())} color="primary" aria-label="Redo" component="label">
+                    <Redo/>
+                </IconButton>
+            </Grid>
+            <Grid xs={1}>
+                <IconButton onClick={() => dispatch(deleteSelectedShape())} color="primary" aria-label="Delete selected shape" component="label">
+                    <Delete/>
+                </IconButton>
+            </Grid>
+        </Grid>
     )
 }
