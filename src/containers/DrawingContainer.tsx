168f53e64ae6b7d63a3a682227bf2601ae7d1c6e
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2022-11-30 19:16:30 -0700

    cleanup

--- src/containers/DrawingContainer.tsx
+++ src/containers/DrawingContainer.tsx
@@ -5,9 +5,9 @@ import Grid from '@mui/system/Unstable_Grid';
 import styled from '@mui/system/styled';
 import Toolbar from "../components/Toolbar";
 import ShapesBar from "../components/ShapesBar";
-import { Stage, Layer } from 'react-konva';
+import {Stage, Layer} from 'react-konva';
 import Rectangle from '../components/shapes/Rectangle'
-import {ShapeTypeEnum, TShapeRect, TShapeText} from "../types";
+import {ShapeTypeEnum, TShapeImage, TShapeRect, TShapeText} from "../types";
 import Konva from 'konva';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../store";
@@ -38,6 +38,65 @@ export default function DrawingContainer() {
         }
     };
 
+    const renderRectangle = (shape: TShapeRect, idx: number) => {
+        return (
+            <Rectangle
+                key={shape.id}
+                shapeProps={shape}
+                isSelected={shape.id === selectedId}
+                onSelect={() => {
+                    dispatch(selectShape(shape.id));
+                }}
+                onChange={(newAttrs: TShapeRect) => {
+                    const shapes = drawingDocument.shapes.slice();
+                    shapes[idx] = newAttrs;
+                    dispatch(updateShapes(shapes))
+                }}
+            />
+        );
+    }
+
+    const renderText = (shape: TShapeText, idx: number) => {
+        return (
+            <TextShape
+                key={shape.id}
+                colour="#FFDAE1"
+                selected={shape.id === selectedId}
+                onSelect={() => {
+                    dispatch(selectShape(shape.id));
+                }}
+                shapeProps={shape}
+                onClick={() => {
+                    dispatch(selectShape(shape.id));
+                }}
+                onChange={(newAttrs: TShapeText) => {
+                    const shapes = drawingDocument.shapes.slice();
+                    shapes[idx] = newAttrs;
+                    dispatch(updateShapes(shapes))
+                }}
+            />
+        )
+    }
+
+    const renderImage = (shape: TShapeImage, idx: number) => {
+        return (
+            <ImageShape
+                key={shape.id}
+                shapeProps={shape}
+                isSelected={shape.id === selectedId}
+                onSelect={() => {
+                    dispatch(selectShape(shape.id));
+                }}
+                onChange={(newAttrs: TShapeRect) => {
+                    const shapes = drawingDocument.shapes.slice();
+                    shapes[idx] = newAttrs;
+                    dispatch(updateShapes(shapes))
+                }}
+            />
+        );
+    }
+
+
     return (
         <Box sx={{flexGrow: 1}}>
             <Grid container spacing={1}>
@@ -60,64 +119,14 @@ export default function DrawingContainer() {
                                 {drawingDocument.shapes.map((shape, i) => {
                                     switch (shape.type) {
                                         case ShapeTypeEnum.rect:
-                                            return (
-                                                <Rectangle
-                                                    key={i}
-                                                    shapeProps={shape}
-                                                    isSelected={shape.id === selectedId}
-                                                    onSelect={() => {
-                                                        dispatch(selectShape(shape.id));
-                                                    }}
-                                                    onChange={(newAttrs: TShapeRect) => {
-                                                        const rects = drawingDocument.shapes.slice();
-                                                        rects[i] = newAttrs;
-                                                        dispatch(updateShapes(rects))
-                                                    }}
-                                                />
-                                            );
+                                            return renderRectangle(shape as TShapeRect, i)
 
                                         case ShapeTypeEnum.text:
-                                            return (
-                                                <TextShape
-                                                    key={i}
-                                                    colour="#FFDAE1"
-                                                    selected={shape.id === selectedId}
-                                                    onSelect={() => {
-                                                        dispatch(selectShape(shape.id));
-                                                    }}
-                                                    shapeProps={shape}
-
-                                                    onClick={() => {
-                                                        dispatch(selectShape(shape.id));
-                                                    }}
-                                                    onChange={(newAttrs: TShapeText) => {
-                                                        const rects = drawingDocument.shapes.slice();
-                                                        rects[i] = newAttrs;
-                                                        dispatch(updateShapes(rects))
-                                                    }}
-
-                                                />
-                                            )
+                                            return renderText(shape as TShapeText, i)
 
                                         case ShapeTypeEnum.image:
-                                            return (
-                                                <ImageShape
-                                                    key={i}
-                                                    shapeProps={shape}
-                                                    isSelected={shape.id === selectedId}
-                                                    onSelect={() => {
-                                                        dispatch(selectShape(shape.id));
-                                                    }}
-                                                    onChange={(newAttrs: TShapeRect) => {
-                                                        const rects = drawingDocument.shapes.slice();
-                                                        rects[i] = newAttrs;
-                                                        dispatch(updateShapes(rects))
-                                                    }}
-                                                />
-                                            );
+                                            return renderImage(shape as TShapeImage, i)
                                     }
-
-
                                 })}
                             </Layer>
                         </Stage>
